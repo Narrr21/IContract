9125495a16481019b7755a9a9d573f061c84bb4d
SebastianHYH <19623180@std.stei.itb.ac.id>
2025-09-27 00:27:35 +0700

    fix db

--- prisma/migrations/20250926172627_cleanup/migration.sql
+++ prisma/migrations/20250926172627_cleanup/migration.sql
@@ -52,7 +52,7 @@ CREATE TABLE "partnership_contracts" (
     "hak1" TEXT NOT NULL,
     "hak2" TEXT NOT NULL,
     "syaratlayanan" TEXT NOT NULL,
-    "nominal" BIGINT NOT NULL,
+    "nominal" INTEGER NOT NULL,
     "tenggatbayar" DATETIME NOT NULL,
     "syaratbayar" TEXT NOT NULL,
     "bank" TEXT NOT NULL,
@@ -60,10 +60,44 @@ CREATE TABLE "partnership_contracts" (
     "norek" TEXT NOT NULL,
     "denda" TEXT NOT NULL,
     "tenggatklaim" DATETIME NOT NULL,
-    "makskompensasi" BIGINT NOT NULL,
+    "makskompensasi" INTEGER NOT NULL,
     "sengketa" TEXT NOT NULL,
     "majeure" TEXT NOT NULL,
     CONSTRAINT "partnership_contracts_kontrakid_fkey" FOREIGN KEY ("kontrakid") REFERENCES "contracts" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "employment_contracts" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "kontrakid" INTEGER NOT NULL,
+    "nomorkontrak" TEXT NOT NULL,
+    "judulkontrak" TEXT NOT NULL,
+    "jeniskontrak" TEXT NOT NULL,
+    "tanggalmulai" DATETIME NOT NULL,
+    "tanggalakhir" DATETIME NOT NULL,
+    "namalengkap" TEXT NOT NULL,
+    "tanggallahir" TEXT NOT NULL,
+    "jeniskelamin" TEXT NOT NULL,
+    "alamatlengkap" TEXT NOT NULL,
+    "nomortelepon" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "posisijabatan" TEXT NOT NULL,
+    "lokasikerja" TEXT NOT NULL,
+    "tanggalmulaikerja" DATETIME NOT NULL,
+    "haricuti" TEXT NOT NULL,
+    "deskripsipekerjaan" TEXT NOT NULL,
+    "detailcuti" TEXT NOT NULL,
+    "aturanlembur" TEXT NOT NULL,
+    "gajipokok" INTEGER NOT NULL,
+    "tunjangantetap" INTEGER NOT NULL,
+    "tunjangantidaktetap" INTEGER NOT NULL,
+    "jaminansosial" TEXT NOT NULL,
+    "fasilitaslain" TEXT NOT NULL,
+    "hukumdanrahasia" TEXT NOT NULL,
+    "disiplin" TEXT NOT NULL,
+    "sanksi" TEXT NOT NULL,
+    "pemutusanhubungankerja" TEXT NOT NULL,
+    CONSTRAINT "employment_contracts_kontrakid_fkey" FOREIGN KEY ("kontrakid") REFERENCES "contracts" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
@@ -71,3 +105,6 @@ CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "partnership_contracts_kontrakid_key" ON "partnership_contracts"("kontrakid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "employment_contracts_kontrakid_key" ON "employment_contracts"("kontrakid");
