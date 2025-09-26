441434c97b07fc0848f86ee066f4a11750500b02
Nadhif <nadhifalrozin@gmail.com>
2025-09-26 14:53:42 +0700

    feat: review

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // --- ADD THIS WEBPACK CONFIGURATION BLOCK ---
+  webpack: (config, { isServer }) => {
+    // This rule is specifically for the server-side build (`isServer` is true)
+    if (isServer) {
+      // Don't bundle `pdfjs-dist` on the server
+      config.externals.push('pdfjs-dist');
+    }
+    return config;
+  },
+  // --- END OF ADDED BLOCK ---
 };
 
 export default nextConfig;
